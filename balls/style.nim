ce3dc6625db8e33cb24fd28724e5e61fec5fc6f9
Smooth Operator <github@andy.disruptek.com>
2023-05-25 11:14:21 -0400

    wip

--- balls/style.nim
+++ balls/style.nim
@@ -1,5 +1,6 @@
-import std/macros
 import std/colors
+import std/macros
+import std/strutils
 
 import grok
 
@@ -65,51 +66,118 @@ const
     Oops: oopsStyle
   ]
 
-when defined(danger): # avoid unused warnings
+when ballsAuditTimeSpace: # avoid unused warnings
   const
     testNumStyle*  = Styling ansiStyleCode(styleItalic) &
                      Styling ansiForegroundColorCode(fgYellow, true)
 
 {.pop.}
 
-let colorThisRun {.used.}: bool =
-  when defined(js) or defined(nimscript):
-    onCI
-  else:
-    onCI or stdmsg().isAtty
+#when nimvm:
+#  # don't try to mess with styling at compile-time
+#  template useColor* = false
+#  const dynamicColor = true
+#else:
+when ballsDry:
+  const useColor* = false
+  const dynamicColor* = false
+  const hasColor = false
+elif defined(js) or defined(nimscript):
+  # don't bother with these yet
+  const useColor* = false
+  const dynamicColor* = false
+  const hasColor = false
+elif onCI:
+  # CI is always safe to color
+  const useColor* = true
+  const dynamicColor* = false
+  const hasColor = true
+else:
+  # else, color only if we're on a tty
+  let hasColor = stdmsg().isAtty
+  #const hasColor = true
+  macro useColor*: untyped = bindSym"hasColor"
+  const dynamicColor* = true
 
-proc useColor*(): bool =
-  ## for the bland folks; they live among us!
-  when ballsDry:
-    false
-  else:
-    when nimvm:
-      # don't try to mess with styling at compile-time
-      true
+proc checkpoint*(ss: varargs[string, `$`]) =
+  ## Like `echo`, but outputs to `stdmsg()` with the other test output.
+  noclobber:
+    when defined(js) or defined(nimscript):
+      echo ss.join(" ")
     else:
-      # at runtime, try to emit style if possible
-      colorThisRun
+      stdmsg().writeLine ss.join(" ")
 
-proc `$`*(style: Styling): string =
-  if useColor():
-    result = style.string
-    if result != resetStyle.string:
-      result = resetStyle.string & result
+proc output*(n: NimNode): NimNode =
+  assert not n.isNil
+  result = newCall(bindSym"checkpoint", n)
 
-proc `&`*(style: Styling; n: NimNode): NimNode =
-  ## combine style and something $able, but only output the
-  ## style if you find that the program is on a tty at runtime
-  let n = dollar n  # this dollar is sometimes gratuitous...
-  if style.string == "":
-    result = n
+proc prefixLines*(s: NimNode; p: string): NimNode =
+  ## prefix each line of multiline input with the given string
+  result = newStmtList()
+  var ss: NimNode
+  case s.kind
+  of nnkCommentStmt:
+    ss = newStmtList()
+    for line in items(splitLines(s.strVal, keepEol = true)):
+      ss.add line.newLit
+  of nnkStmtList:
+    ss = s
   else:
-    result = nnkIfStmt.newTreeFrom n:
-      nnkElifBranch.newTreeFrom n:
-        bindSym"useColor".newCall
-        nestList ident"&":
-          nnkStmtList.newTreeFrom n:
-            newLit $style
-            n
-            newLit $resetStyle
-      nnkElse.newTree:
-        n
+    ss = newStmtList s
+  for line in ss.items:
+    result.add infix(p.newLit, "&", line)
+  result = nestList(ident"&", result)
+
+when dynamicColor:
+  proc `$`*(style: Styling): string =
+    result = style.string
+    if hasColor:
+      if not result.startsWith(resetStyle.string):
+        result = resetStyle.string & result
+
+  proc `&`*(style: Styling; n: NimNode): NimNode =
+    ## combine style and something $able, but only output the
+    ## style if you find that the program is on a tty at runtime
+    if style.string == "":
+      n # this dollar is sometimes gratuitous...  FIXME: wut.
+    else:
+      nnkIfStmt.newTreeFrom n:
+        nnkElifBranch.newTreeFrom n:
+          bindSym"hasColor"
+          nestList ident"&":
+            nnkStmtList.newTreeFrom n:
+              newLit style.string
+              n
+              newLit resetStyle.string
+        nnkElse.newTree:
+          n
+
+  macro report*(n: string) =
+    ## render a multi-line comment
+    var prefix = $lineNumStyle & "## " & $commentStyle
+    var postfix = newLit resetStyle.string
+    result =
+      nnkIfStmt.newTreeFrom n:
+        nnkElifExpr.newTreeFrom n:
+          bindSym"useColor"
+          output infix(prefixLines(n, prefix), "&", postfix)
+        nnkElse.newTree:
+          output prefixLines(n, "## ")
+
+else:
+  when useColor:
+    proc `$`*(style: Styling): string =
+      result = style.string
+      if not result.startsWith(resetStyle.string):
+        result = resetStyle.string & result
+    proc `&`*(style: Styling; n: NimNode): NimNode =
+      nestList ident"&":
+        nnkStmtList.newTreeFrom n:
+          newLit style.string
+          dollar n
+          newLit resetStyle.string
+      dollar n
+  else:
+    template `$`*(style: Styling): string = ""
+    proc `&`*(style: Styling; n: NimNode): NimNode = n
+    macro report*(n: string) = output prefixLines(n, "## ")
